e025fbc631464a27c527e433dd0ae93c2c42f5a4
pavithraboopathi24 <32186708+pavithraboopathi24@users.noreply.github.com>
2018-06-22 15:22:16 +0530

    Create G8.java

---
+++ G8.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class G8{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(((n*n+n)/2));
+        
+    }
+}
